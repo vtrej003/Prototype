66601349f7c8a38410f13b6a041606879ba66d3a
root <vtrej003@ucr.edu>
2020-08-09 22:30:17 -0700

    upload Unity prototype project

---
+++ Assets/Prototype/Selector/SelectionManager.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class SelectionManager : MonoBehaviour
+{
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private Material highlightMaterial;
+    [SerializeField] private Material defaultMaterial;
+    [SerializeField] public GameObject teleportObject;
+    private Transform _selection;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_selection != null)
+        {
+            Renderer selectionRenderer = _selection.GetComponent<Renderer>();
+            selectionRenderer.material = defaultMaterial;
+            _selection = null;
+        }
+
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Transform selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                Renderer selectionRenderer = selection.GetComponent<Renderer>();
+                Collider selectionCollider = selection.GetComponent<Collider>();
+                teleportObject.transform.position = selectionCollider.bounds.center;
+                if (selectionRenderer != null)
+                {
+                    defaultMaterial = selectionRenderer.material;
+                    selectionRenderer.material = highlightMaterial;
+                    
+
+                }
+
+                
+            }
+        }
+    }
+}
---
+++ Assets/Prototype/Spells/Spell.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class Spell : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private string selectableTag = "Selectable";
+    [SerializeField] private ParticleSystem spell;
+    public SteamVR_Action_Boolean spellAction;
+    public SteamVR_Behaviour_Pose pose;
+    
+    void Awake()
+    {
+        pose = GetComponent<SteamVR_Behaviour_Pose>();
+    }
+    
+    void Start()
+    {
+        
+    }
+    
+    
+    void CastSpell()
+    {
+        RaycastHit hit;
+        Vector3 dir = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.DrawRay(transform.position, dir * hit.distance, Color.yellow);
+            Transform selection = hit.transform;
+            if (selection.CompareTag(selectableTag))
+            {
+                Collider selectionCollider = selection.GetComponent<Collider>();
+                spell.transform.position = selectionCollider.bounds.center;
+                spell.Play();
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 dir = transform.TransformDirection(Vector3.forward);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            Debug.DrawRay(transform.position, dir * hit.distance, Color.yellow);
+        }
+        
+        if (spellAction.GetStateUp(pose.inputSource))
+        {
+            print("this is right controller cast");
+            CastSpell();
+        }
+    }
+}
